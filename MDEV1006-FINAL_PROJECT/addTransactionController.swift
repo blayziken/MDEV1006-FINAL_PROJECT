a88a686211d882463a6771e316a2353aeb9d16d3
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2024-04-04 18:50:21 -0400

    View Controller

--- MDEV1006-FINAL_PROJECT/addTransactionController.swift
+++ MDEV1006-FINAL_PROJECT/addTransactionController.swift
@@ -6,8 +6,22 @@
 //
 
 import Foundation
-
+import UIKit
 
 class addTransactionController: UIViewController, UITableViewDelegate {
+    
+    
+    //MARK: IB OUTLETS
+    @IBOutlet weak var amountOutlet: UITextField!
+    @IBOutlet weak var noteOutlet: UITextField!
+    @IBOutlet weak var categoryOutlet: UITextField!
+ 
+    
+    //MARK: IB ACTIONS
+    @IBAction func saveBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearBtn(_ sender: UIButton) {
+    }
     
 }
